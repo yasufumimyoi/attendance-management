120d84775ff1f39987824d7a46f7b6659c812287
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-12-03 22:02:06 +0900

    playing with react-hooks-form

--- my-app/src/components/EmployeesAddForm.tsx
+++ my-app/src/components/EmployeesAddForm.tsx
@@ -1,16 +1,19 @@
-//react hook formを入れたい
+import { useForm } from "react-hook-form";
+
 //データベースにinsertさせる
 //追加後ダッシュボード画面に移動させる
 
 const EmployeesAddForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data: any) => console.log(data);
   return (
     <div>
       <h3>新規従業員追加</h3>
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <button>追加</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="name" ref={register} />
+        <input name="age" ref={register({ required: true })} />
+        {errors.age && <span>This field is required</span>}
+        <input type="submit" />
       </form>
     </div>
   );
